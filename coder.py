984f3066bef80f68c3059247ca1828283c0d493b
Omar Travieso <omartravieso@ufl.edu>
2023-10-31 17:20:50 -0400

    Decode program added

--- coder.py
+++ coder.py
@@ -2,8 +2,28 @@ def encode(raw_password):
     encoded_password = ""
     for c in raw_password:
         i = int(c) + 3
+        if i == 10:
+            i = 0
+        elif i == 11:
+            i = 1
+        elif i == 12:
+            i = 2
         encoded_password += str(i)
     return encoded_password
+
+ # initialize decode() method
+def decode(encoded_password):
+    decoded_password = ""
+    for x in encoded_password:
+        e = int(x) - 3
+        if e == -3:
+            e = 7
+        elif e == -2:
+            e = 8
+        elif e == -1:
+            e = 9
+        decoded_password += str(e)
+    return decoded_password
 
 def main():
     flag = True
@@ -22,6 +42,8 @@ def main():
             print("Your password has been encoded and stored!\n")
         if user_choice == 2:
             print("The partner completes this part")
+            decoded_password = decode(stored_password)
+            print(f'The encoded password is {stored_password}, and the original password is {decoded_password}.\n')
         if user_choice == 3:
             flag = False
 
